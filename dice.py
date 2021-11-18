dc173f53f637dcb793d446802abfa0b38722c8b1
jfoster19767 <57021056+jfoster19767@users.noreply.github.com>
2021-11-17 22:18:15 -0700

    Update dice.py

--- dice.py
+++ dice.py
@@ -4,43 +4,53 @@ import discord
 
 async def dice(client, message):
 	#First we should filter messages that start with roll but is unrelated to dice
-	message_list = message.content.split(' ')
-	if len(message_list) == 2:
-		#Now I need to figure out how many dice I'm rolling
-		if message_list[1][0].isdigit():
-		#rolling multiple dice
-			roll_string = message_list[1].split('d')
-			number_of_dice = roll_string[0]
-			number_of_dice = int(number_of_dice)
-			type_of_dice = roll_string[1]
-			type_of_dice = int(type_of_dice)
-			# now roll that die and add the result
-			result_list = [None] * number_of_dice
-			for x in range(number_of_dice):
-				result_list[x - 1] = random.randint(1, type_of_dice)
-			await message.channel.send(str(result_list) + " = " + str(sum(result_list)))
-				
-		else:
-			if message.content.lower() == 'roll d2':
-				#throw a coin
-				result = random.randint(1,2)
-				await message.channel.send(file=discord.File('Dice_bag/d2_' + str(result) + '.png'))
+	input_args = message.content.split(' ')
+	
+	output_string = ""
+	total = 0
 
-			elif message.content.lower() == 'roll d4':
-				result = random.randint(1,4)
-				await message.channel.send(file=discord.File('Dice_bag/d4_' + str(result) + '.png'))
+	for i in range(1, len(input_args)):
+			roll_args = str.split(input_args[i], "d")
+			if "+" in roll_args[1]:
+					operator_args = str.split(roll_args[1], "+")
 
-			elif message.content.lower() == 'roll d6':
-				result = random.randint(1,6)
-				await message.channel.send(file=discord.File('Dice_bag/d6_' + str(result) + '.png'))
+					output_string = output_string + " ("
 
-			elif message.content.lower() == 'roll d8':
-				result = random.randint(1,8)
-				await message.channel.send(file=discord.File('Dice_bag/d8_' + str(result) + '.png'))
-			#generic catch all for any other dice
+					for j in range(0, int(roll_args[0])):
+							roll = random.randint(1,int(operator_args[0]))
+							total = total + roll
+							output_string = output_string + str(roll) + " + "
+
+					if len(operator_args) != 1:
+							output_string = output_string + operator_args[1] + ") +"
+							total = total + int(operator_args[1])
+					else:
+							output_string = output_string[0:-3] + ") +"
+			elif "-" in roll_args[1]:
+					operator_args = str.split(roll_args[1], "-")
+
+					output_string = output_string + " ("
+
+					for j in range(0, int(roll_args[0])):
+							roll = random.randint(1,int(operator_args[0]))
+							total = total + roll
+							output_string = output_string + str(roll) + " + "
+
+					if len(operator_args) != 1:
+							output_string = output_string[0:-3] + " - " + operator_args[1] + ") +"
+							total = total - int(operator_args[1])
+					else:
+							output_string = output_string[0:-3] + ") +"
 			else:
-				result = random.randint(1, int(message.content[-1]))
-				await message.channel.send(str(result))
+					output_string = output_string + " ("
 
-	else:
-		return
+					for j in range(0, int(roll_args[0])):
+							roll = random.randint(1,int(roll_args[1]))
+							total = total + roll
+							output_string = output_string + str(roll) + " + "
+							
+					output_string = output_string[0:-3] + ") +"
+					
+	output_string = output_string[1:-2]
+	output_string = output_string + " = " + str(total)		
+	await message.channel.send(output_string)
